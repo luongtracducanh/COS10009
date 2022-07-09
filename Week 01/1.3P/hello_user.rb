ec87e8c315b22452544395d4d53bdae2a1817d20
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-07-09 16:01:23 +0700

    Update hello_user.rb

--- Week 01/1.3P/hello_user.rb
+++ Week 01/1.3P/hello_user.rb
@@ -14,15 +14,15 @@ def hello()
 	name = gets()
 	puts('Your name is ' + name + '!')
 	puts('What is your family name?')
-	family_name = gets()
-	puts('Your family name is: '+ family_name.gsub(/\s+/, "") + '!')
+	family_name = gets.chomp()
+	puts("Your family name is: #{family_name}!")
 	puts('What year were you born?')
-	year_born = gets().to_i
+	year_born = gets()
 	# Calculate the users age
-	age = Date.today.year - year_born
+	age = Date.today.year - year_born.to_i()
 	puts('So you are ' + age.to_s + ' years old')
 	puts('Enter your height in metres (i.e as a float):')
-	value = gets().to_f # Should this be a float or an Integer? Why?
+	value = gets.to_f() # Should this be a float or an Integer? Why?
 	value = value * INCHES
 	puts('Your height in inches is:')
 	puts(value.to_s())
